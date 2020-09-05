1c5815a5fec54e7098d5eb19796fae240a16bad8
Elena-brv <leena.brv@gmail.com>
2020-09-05 20:54:31 +0300

    Updates

---
+++ precache-manifest.77d66764b256a590d0c3c4f8f873dd5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a543e4bfb573e4249a98e5140547afa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c35de27807e4e9a56e0b",
+    "url": "./static/css/main.7476825b.chunk.css"
+  },
+  {
+    "revision": "f80f4bcc18d682b111a2",
+    "url": "./static/js/2.3114ea5a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.3114ea5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35de27807e4e9a56e0b",
+    "url": "./static/js/main.4e750d22.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  },
+  {
+    "revision": "17d0b0a1a2ba9d26444b0339836bc2d5",
+    "url": "./static/media/wooden-boards.17d0b0a1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.77d66764b256a590d0c3c4f8f873dd5f.js"
 );
 
 self.addEventListener('message', (event) => {
